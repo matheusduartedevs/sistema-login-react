e1494eac5aacca25ada03c8ad5aae7bcf5d41687
unknown <matheus.duarte42@gmail.com>
2023-06-19 08:24:21 -0300

    adição das pages e routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import GlobalStyle from './styles/global'
+import RoutesApp from './routes'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+        <RoutesApp />
+        <GlobalStyle />
+    </>
   )
 }
 
